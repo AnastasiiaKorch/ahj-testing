5964d50d806a9b7a68782f296717f15bb79cad97
AnastasiiaKorch <anastasia_92_92@list.ru>
2023-02-27 15:27:51 +0300

    task added

---
+++ src/js/__tests__/logic.test.js
@@ -0,0 +1,18 @@
+import luhnAlgorithm from '../algorithm.js';
+import  paymentMethod from '../payment-method.js';
+
+test('luhn algorithm check should return true', () => {
+  expect(luhnAlgorithm('4893 4702 1277 8911')).toBe(true);
+});
+
+test('luhn algorithm check should return false', () => {
+  expect(luhnAlgorithm('4893 4702 1277 8912')).toBe(false);
+});
+
+test('should return visa result as a payment method', () => {
+  expect(paymentMethod(4893470212778911)).toBe('visa');
+});
+
+test('should return mastercard result as a payment method', () => {
+  expect(paymentMethod(5432207943936151)).toBe('master');
+});
---
+++ src/js/payment-method.js
@@ -0,0 +1,29 @@
+export default function paymentMethod(value) {
+  const mir = new RegExp('^220(0|4)[0-9]{12}[0-9]*$');
+  const visa = new RegExp('^4[0-9]{12}(?:[0-9]{3})?$');
+  const master = new RegExp('^5[1-5][0-9]{14}$');
+  const master2 = new RegExp('^2[2-7][0-9]{14}$');
+  const amex = new RegExp('^3[47][0-9]{13}$');
+
+  if (mir.test(value)) {
+    return 'mir';
+  }
+
+  if (visa.test(value)) {
+    return 'visa';
+  }
+
+  if (master.test(value)) {
+    return 'master';
+  }
+
+  if (master2.test(value)) {
+    return 'master';
+  }
+
+  if (amex.test(value)) {
+    return 'amex';
+  }
+
+  return undefined;
+}
